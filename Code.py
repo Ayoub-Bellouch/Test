f96efe744c1d331e8d972b70528c9ea7f70007aa
Ayoub BELLOUCH <a.b.bellouch@gmail.com>
2021-08-24 17:14:22 +0100

    main changement

--- Code.py
+++ Code.py
@@ -5,4 +5,12 @@ print("Salut Ayoub ")
 print("This is a some one's else push !! \n u should find this one .. ! ")
 
 
+
+
+
+
+
 #  le serveur de notre programme needs more updates on the networking edge !
+
+
+#  et voila le Main changement !
