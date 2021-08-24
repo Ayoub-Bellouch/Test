0757790ca49f4a980b7cfc29b9e3efdff14423c2
Ayoub BELLOUCH <a.b.bellouch@gmail.com>
2021-08-24 15:27:29 +0100

    comment about updating networking inside the server

--- Code.py
+++ Code.py
@@ -3,3 +3,6 @@ print("Salut Ayoub ")
 
 
 print("This is a some one's else push !! \n u should find this one .. ! ")
+
+
+#  le serveur de notre programme needs more updates on the networking edge !
